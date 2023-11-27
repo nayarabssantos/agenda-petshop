483c8f9fb1a8ea3cb1192bae501ae795a8f33def
Nayara Bruna Silva dos Santos <nayarabrunasilvadossantos@fico.com>
2023-11-27 16:00:58 -0300

    creating table and adding data into it

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,12 +1,14 @@
-
+const Atendimento = require('../models/atendimentos')
 
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Você está na roda de atendimentos e está realizando um GET'))
 
     app.post('/atendimentos', (req, res) =>{
 
-        console.log(req.body)
+        const atendimento = req.body
 
-         res.send('Vocês está na roda de atendimentos e está realizando um POST')
+        Atendimento.adiciona(atendimento)
+
+         res.send('Post atendimento')
     })
 }
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const customExpress = require('./config/customExpress')
 const conexao = require('./infraestrutura/conexao')
+const tabelas = require('./infraestrutura/tabelas')
 
 conexao.connect(erro => {
     if (erro) {
         console.log(erro)
     } else{
         console.log('conectado com sucesso')
+
+        tabelas.init(conexao)
 
         const app = customExpress()
 
