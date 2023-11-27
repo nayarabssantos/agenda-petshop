fb7909e2d123f4640119c81bf91af087ef70e097
Nayara Bruna Silva dos Santos <nayarabrunasilvadossantos@fico.com>
2023-11-27 17:52:37 -0300

    changing the returno of method post

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -37,7 +37,7 @@ class Atendimento {
             if (erro) {
                 res.status(400).json(erro)
             } else {
-                res.status(201).json(resultados)
+                res.status(201).json(atendimento)
             }
         })
         }
