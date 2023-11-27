68e459d0250995ab48a5dfb8118e69545adf4bad
Nayara Bruna Silva dos Santos <nayarabrunasilvadossantos@fico.com>
2023-11-27 16:26:41 -0300

    adding moment libary to handle the date fields of atendimento table

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,11 +1,16 @@
+const moment = require('moment')
+
 const atendimentos = require('../controllers/atendimentos')
 const conexao = require('../infraestrutura/conexao')
 
 class Atendimento{
     adiciona(atendimento){
+        const data_criacao = moment().format('YYYY-MM-DD HH:MM:SS')
+        const data_atendimento = moment(atendimento.data_atendimento, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        const atendimentoDatado = {... atendimento, data_criacao, data_atendimento}
         const sql = 'INSERT INTO atendimentos SET ?'
 
-        conexao.query(sql, atendimento, (erro, resultados) => {
+        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
             if(erro){
                 console.log(erro)
             }else{
