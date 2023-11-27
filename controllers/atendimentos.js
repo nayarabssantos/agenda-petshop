22982a13861604c81993308f063156e5b20829e6
Nayara Bruna Silva dos Santos <nayarabrunasilvadossantos@fico.com>
2023-11-27 14:47:43 -0300

    adding post method and body-parser

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,5 +1,12 @@
 
 
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send('Você está na roda de atendimentos e está realizando um get'))
+    app.get('/atendimentos', (req, res) => res.send('Você está na roda de atendimentos e está realizando um GET'))
+
+    app.post('/atendimentos', (req, res) =>{
+
+        console.log(req.body)
+
+         res.send('Vocês está na roda de atendimentos e está realizando um POST')
+    })
 }
